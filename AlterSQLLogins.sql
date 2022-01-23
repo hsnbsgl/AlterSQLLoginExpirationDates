38f83260abb4bfb2afe9c71075242413f940381c
hsnbsgl <34284320+hsnbsgl@users.noreply.github.com>
2022-01-23 23:48:36 +0300

    Update AlterSQLLogins.sql

--- AlterSQLLogins.sql
+++ AlterSQLLogins.sql
@@ -5,7 +5,7 @@ SELECT @@SERVERNAME AS ServerName,
         ISNULL(CONVERT (varchar (100) ,DATEADD (dd, CONVERT(int, LOGINPROPERTY (SL.name,'DaysUntilExpiration')),
                                                 CONVERT(int,LOGINPROPERTY(SL.name, 'PasswordLastSetTime'))), 101),'Never Expire') AS PasswordExpirationDate,
         CASE WHEN is_expiration_checked = 1 THEN 'TRUE' ELSE 'FALSE' END AS PasswordExpireChecked
- FROM sys.sq]_logins AS SL
+ FROM sys.sql_logins AS SL
  WHERE SL.name NOT LIKE '##%' AND SL.name NOT LIKE 'endPointUser' and is disabled = 0
  ORDER BY (LOGINPROPERTY(SL.name,'PasswordLastSetTime')) DESC
  
@@ -13,7 +13,7 @@ SELECT @@SERVERNAME AS ServerName,
  
  /*ALTER Login with same password*/
  
- SELECT 'ALTER LOGIN [+ P.name + '] WITH PASSWORD=0x' +
+ SELECT 'ALTER LOGIN ['+ P.name + '] WITH PASSWORD=0x' +
  CONVERT (VARCHAR (500) , cast (convert (sysname, LoginProperty (p.name, 'PasswordHash')) AS varbinary (256)), 2) + ' HASHED '+ 
  ' ,DEFAULT DATABASE=['+P.default_database_name +']'+
  ' ,DEFAULT LANGUAGE=['+P.default_language_name +']'+
@@ -21,11 +21,11 @@ SELECT @@SERVERNAME AS ServerName,
  ' , CHECK EXPIRATION= OFF' 
  FROM sys.server principals p
  LEFT JOIN sys.sql_logins L ON P.sid = L.sid
- WHERE P.type = 'S' AND P.name NOT LIKE '##%##' AND p.is disabled=0 -SOL LOGIN
+ WHERE P.type = 'S' AND P.name NOT LIKE '##%##' AND p.is disabled=0 
  ORDER BY P.name
  
  /*SET Check policy ON, check expiration ON*/
  
  SELECT 'ALTER LOGIN [' + p.name + '] WITH CHECK_POLICY= ON , CHECK_ EXPIRATION=ON ' FROM sys.server_principals p
- WHERE P.type= 'S' AND P.name NOT LIKE '##%##' AND p.is_disabled=0 -SOL LOGIN
+ WHERE P.type= 'S' AND P.name NOT LIKE '##%##' AND p.is_disabled=0 
  ORDER BY P.name
